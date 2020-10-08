eeab41c971a0bb763f22aad16d9ef5fca096fc30
tsuguhide <tsuguhide.n@gmail.com>
2020-10-08 19:32:53 +0900

    マイグレーション修正

--- db/migrate/20201006100555_create_tasks.rb
+++ db/migrate/20201006100555_create_tasks.rb
@@ -3,10 +3,11 @@ class CreateTasks < ActiveRecord::Migration[6.0]
     create_table :tasks do |t|
       t.string  :title,            null: false
       t.text    :title_detail
-      t.integer :category_id,      null: false
-      t.string  :category_detail
+      # t.integer :category_id,      null: false
+      # t.string  :category_detail
       t.string  :task,             null: false
       t.text    :task_memo
+      t.references :user
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,12 @@ ActiveRecord::Schema.define(version: 2020_10_06_100555) do
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "title_detail"
-    t.integer "category_id", null: false
-    t.string "category_detail"
     t.string "task", null: false
     t.text "task_memo"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
